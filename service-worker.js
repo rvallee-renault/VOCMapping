7e1ef92c113d73fafbe839b50e417d3da37a48a4
rvallee-renault <valleerichard78@gmail.com>
2026-07-15 09:34:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,19 +2,20 @@ const CACHE_NAME = "vocmapping-v1";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
